eaf4f746a4b7ea04738716aad0fc691f870da961
allen chung <east11@gmail.com>
2020-09-07 15:17:48 +0800

    understand better the params

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,17 +2,13 @@
 class AnswersController < ApplicationController
 
 
-
-
   def answer
-    if params[:coach_answer]
-      @question.include "?"
+    if params[:question].include? "?"
       @coach_answer = "Silly question, get dressed and go to work!"
-    elsif
-      @question == "I am going to work"
+    elsif params[:question] == "I am going to work"
       @coach_answer = "Great!"
     else
-      return "I don't care, get dressed and go to work!"
+      @coach_answer = "I don't care, get dressed and go to work!"
     end
   end
 end
